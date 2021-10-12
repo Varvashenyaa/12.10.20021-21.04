11b95fbec7ac8d5b8e3b6d38f8b260ce2f4d0d40
Varvashenyaa <alena.varvashenya@gmail.com>
2021-10-12 21:12:17 +0300

    add task 1

---
+++ месяц/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int o;
+	cout << "Введите номер месяца"<<endl; 
+	cin >> o;
+	while (o < 0 || o>12)
+	{
+		cout << "Такого месяца нет, введите значение от 1 до 12:" << endl;
+		cin >> o;
+	}
+	switch (o) {
+	case 1: cout << "Январь"<<endl << "Зима"; break; 
+	case 2: cout << "Февраль" << endl << "Зима"; break;
+	case 3: cout << "Март" << endl << "Весна"; break;
+	case 4: cout << "Апрель" << endl << "Весна"; break;
+	case 5: cout << "Май" << endl << "Весна"; break;
+	case 6: cout << "Июнь" << endl << "Лето"; break;
+	case 7: cout << "Июль" << endl << "Лето"; break;
+	case 8: cout << "Август" << endl << "Лето"; break;
+	case 9: cout << "Сентябрь" << endl << "Осень"; break;
+	case 10: cout << "Октябрь" << endl << "Осень"; break;
+	case 11: cout << "Ноябрь" << endl << "Осень"; break;
+	case 12: cout << "Декабрь" << endl << "Зима"; break;
+
+	}
+}
